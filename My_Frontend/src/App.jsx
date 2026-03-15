047bd752f286c316c0b43bf431a635da2fb3ed05
Suraj Yadav <surajyadav2823510@gmail.com>
2026-03-15 12:35:25 +0530

    added task list in UI

--- My_Frontend/src/App.jsx
+++ My_Frontend/src/App.jsx
@@ -2,12 +2,14 @@ import './style/App.css'
 import NavBar from './components/NavBar'
 import { Routes,Route } from 'react-router-dom'
 import AddTask from './components/AddTask'
+import List from './components/List'
+
 function App() {
   return (
     <>
     <NavBar/>
      <Routes>
-        <Route path ='/' element={<h1>Task List</h1>}></Route>
+        <Route path ='/' element={<List />}></Route>
         <Route path ='/add' element={<AddTask />}></Route>
      </Routes>
     </>
---
+++ My_Frontend/src/components/List.jsx
@@ -0,0 +1,65 @@
+import { useState, useEffect } from "react";
+
+
+export default function List() {
+
+    const [taskData, setTaskData] = useState([]);
+
+    useEffect(() => {
+        getListData();
+    }, []);
+
+    const getListData = async () => {
+
+        let list = await fetch(
+            "http://localhost:3200/todo/tasks"
+        );
+
+        list = await list.json();
+
+        setTaskData(list);
+
+    };
+
+    return (
+
+        <div className="listContainer">
+
+            <h1 className="listTitle">
+                To Do List
+            </h1>
+
+            <table className="todoTable">
+
+                <thead>
+                    <tr>
+                        <th>S.No</th>
+                        <th>Title</th>
+                        <th>Description</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+
+                    {
+                        taskData.map((item, index) => (
+
+                            <tr key={item._id}>
+
+                                <td>{index + 1}</td>
+                                <td>{item.title}</td>
+                                <td>{item.description}</td>
+
+                            </tr>
+
+                        ))
+                    }
+
+                </tbody>
+
+            </table>
+
+        </div>
+
+    );
+}
